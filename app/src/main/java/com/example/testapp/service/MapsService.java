0fb50469c8e216d8e121b6d8254ca8a2eb82c2cb
Tim <sujamh0524@gmail.com>
2020-08-10 16:59:10 +0800

    Added restTemplate, circleoptions

---
+++ app/src/main/java/com/example/testapp/service/MapsService.java
@@ -0,0 +1,22 @@
+package com.example.testapp.service;
+
+import org.springframework.web.client.RestTemplate;
+
+public class MapsService {
+
+    private RestTemplate restTemplate;
+
+    public MapsService(){
+        restTemplate = new RestTemplate();
+    }
+
+
+    public void postLocation(){
+
+    }
+
+    public void getLocations(){
+
+    }
+
+}
--- app/src/main/java/com/example/testapp/util/GoogleMapUtil.java
+++ app/src/main/java/com/example/testapp/util/GoogleMapUtil.java
@@ -2,12 +2,14 @@ package com.example.testapp.util;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 
 import com.example.testapp.R;
 import com.example.testapp.model.LocationObject;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -27,7 +29,7 @@ public class GoogleMapUtil {
     // TODO Add parameter for Area POJO parsed from json
     public static void populateLocationMarkers(GoogleMap mMap) {
         List<LocationObject> dummyLocations = new ArrayList<>();
-        LocationObject loc1 = new LocationObject(14.555727, 121.021749);
+        LocationObject loc1 = new LocationObject(14.5611721636, 121.021212863);
         LocationObject loc2 = new LocationObject(14.555267, 121.022352);
         dummyLocations.add(loc1);
         dummyLocations.add(loc2);
@@ -36,8 +38,16 @@ public class GoogleMapUtil {
         Bitmap customIcon = getCustomIcon();
         for (LocationObject l : dummyLocations) {
             LatLng latLng = new LatLng(l.getLatitude(), l.getLongitude());
-            MarkerOptions markerOptions = new MarkerOptions().position(latLng)
-                    .icon(BitmapDescriptorFactory.fromBitmap(customIcon));
+            CircleOptions circleOptions = new CircleOptions();
+            circleOptions.center(latLng);
+            circleOptions.strokeWidth(4);
+            circleOptions.strokeColor(Color.argb(255, 255, 0 , 0));
+            circleOptions.fillColor(Color.argb(32, 255, 0 , 0));
+            circleOptions.radius(30);
+            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Latitude: " + l.getLatitude() + " Longitude: " + l.getLongitude());
+           /* MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Latitude: " + l.getLatitude() + " Longitude: " + l.getLongitude())
+                    .icon(BitmapDescriptorFactory.fromBitmap(customIcon));*/
+           mMap.addCircle(circleOptions);
             Marker marker = mMap.addMarker(markerOptions);
             hotspotMarkerList.add(marker);
         }
